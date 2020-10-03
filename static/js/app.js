e715a04b5b70e97edc36507708e4a94a2df47ddf
audric <audricperger@gmail.com>
2020-10-02 22:30:42 -0700

    thanks paul!

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,5 @@
 // Get a reference to the table body
+
 const tbody = d3.select("tbody");
 
 // Console.log the weather data from data.js
@@ -10,20 +11,28 @@ data.forEach(ufoSighting => console.log(ufoSighting));
 // // Step 2:  Use d3 to append one table row `tr` for each weather report object
 // Don't worry about adding cells or text yet, just try appending the `tr` elements.
 function loadData(data) {
+
+  tbody.selectAll("*").remove()
   data.forEach(ufoSighting => {
     console.log(ufoSighting);
-    let row = tbody.append("tr");
+    const row = tbody.append("tr");
+    Object.values(ufoSighting).forEach(value => {
+      // Append a cell to the row for each value
+      // in the weather report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
 }
 
-loadData(data)
+loadData(data);
 
 // // // Step 3:  Use `Object.entries` to consol.log each weather report value
-data.forEach(ufoSighting => {
-  let row = tbody.append("tr");
-  console.log(Object.values(ufoSighting))
-  console.log(Object.entries(ufoSighting))
-});
+// data.forEach(ufoSighting => {
+//   let row = tbody.append("tr");
+//   console.log(Object.values(ufoSighting))
+//   console.log(Object.entries(ufoSighting))
+// });
 
 // // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
 // data.forEach(ufoSighting => {
@@ -38,19 +47,21 @@ data.forEach(ufoSighting => {
 
 // // // Step 5: Use d3 to update each cell's text with
 // // // weather report values (weekday, date, high, low)
-data.forEach(ufoSighting => {
-  let row = tbody.append("tr");
-  Object.values(ufoSighting).forEach(value => {
-    // Append a cell to the row for each value
-    // in the weather report object
-    var cell = row.append("td");
-    cell.text(value);
-  });
-});
 
-d3.select("#button").on("click", () => {
-  const input = d3.select("#datetime").attr("value")
-})
+
+d3.select("#filter-btn").on("click", () => {
+  const input = d3.select("#datetime").property("value");
+  const filteredData = [];
+  data.forEach(ufoSighting => {
+    
+    if (ufoSighting.datetime === input) {
+      console.log("bla")
+      filteredData.push(ufoSighting);
+    }
+  });
+  console.log("weeeee", filteredData);
+  loadData(filteredData);
+});
 
 
 // expected output: true
@@ -62,5 +73,3 @@ d3.select("#button").on("click", () => {
 // //     cell.text(value);
 // //   });
 // // });
-
-input
