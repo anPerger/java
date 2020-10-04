d02551a457e50fb83cbc4dd1f32012c813566f56
audric <audricperger@gmail.com>
2020-10-04 10:24:20 -0700

    'cleaning'

--- static/js/app.js
+++ static/js/app.js
@@ -27,26 +27,7 @@ function loadData(data) {
 
 loadData(data);
 
-// // // Step 3:  Use `Object.entries` to consol.log each weather report value
-// data.forEach(ufoSighting => {
-//   let row = tbody.append("tr");
-//   console.log(Object.values(ufoSighting))
-//   console.log(Object.entries(ufoSighting))
-// });
 
-// // // Step 4: Use d3 to append 1 cell per weather report value (weekday, date, high, low)
-// data.forEach(ufoSighting => {
-//   let row = tbody.append("tr");
-//   Object.values(ufoSighting).forEach(value => {
-//     console.log(value);
-//     // Append a cell to the row for each value
-//     // in the weather report object
-//     let cell = row.append("td");
-//   });
-// });
-
-// // // Step 5: Use d3 to update each cell's text with
-// // // weather report values (weekday, date, high, low)
 
 
 d3.select("#filter-btn").on("click", () => {
